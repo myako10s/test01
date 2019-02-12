b00340874217f4cbf31f74627056e99d1fd135a4
myako10s <myako10s@gmail.com>
2019-02-13 01:30:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,20 @@
 $(document).ready(function() {
     var rec = new webkitSpeechRecognition();
-        rec.continuous = true;
-        rec.interimResults = false;
-        rec.lang = 'ja-JP';
+    rec.continuous = true;
+    rec.interimResults = false;
+    rec.lang = 'ja-JP';
 
-        var userSaid = function(str, s) {
-            return str.indexOf(s) > -1;
+    rec.onresult = function(e) {
+        rec.stop();
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                var str = e.results[i][0].transcript;
+                $("#log").append( str + "<br />" );
+                $("#log").scrollTop( $("#log")[0].scrollHeight );
+                console.log('Recognised: ' + str);
+            }
         }
-
-        rec.onresult = function(e) {
-            rec.stop();
-            for (var i = e.resultIndex; i < e.results.length; ++i) {
-                if (e.results[i].isFinal) {
-                        var str = e.results[i][0].transcript;
-                        console.log('Recognised: ' + str);
-                        if (userSaid(str, '赤')) {
-                            $('#box').css("background-color","red");
-                }
-                            else if (userSaid(str, '青')) {
-                            $('#box').css("background-color","blue");
-                }
-                            else if (userSaid(str, '黄色')) {
-                                $('#box').css("background-color","yellow");
-                            }
-                            else if (userSaid(str, '緑')) {
-                                $('#box').css("background-color","green");
-                                            }
-
-                                        }
-                                    }
-                            }
+    }
 
     rec.onstart = () => { console.log('on start') };
     rec.onend = () => { console.log('on end'); rec.start(); };
