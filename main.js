b23684c687844d29a1d18b58ece3935b28decdb9
myako10s <myako10s@gmail.com>
2019-02-13 01:14:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
         }
 
         rec.onresult = function(e) {
+            rec.stop();
             for (var i = e.resultIndex; i < e.results.length; ++i) {
                 if (e.results[i].isFinal) {
                         var str = e.results[i][0].transcript;
@@ -30,6 +31,18 @@ $(document).ready(function() {
                                     }
                             }
 
+    rec.onstart = () => { console.log('on start') };
+    rec.onend = () => { console.log('on end'); rec.start(); };
+
+    rec.onspeechstart = () => { console.log('on speech start') };
+    rec.onspeechend = () => { console.log('on speech end') };
+
+    rec.onosundstart = () => { console.log('on sound start') };
+    rec.onsoundend = () => { console.log('on sound end') };
+
+    rec.onaudiostart = () => { console.log('on audio start') };
+    rec.onaudioend = () => { console.log('on audio end') };
+    
     $("#startRecBtn").click(function() {
       rec.start();
     });
@@ -37,3 +50,4 @@ $(document).ready(function() {
       rec.stop();
     });
 });
+rec.start()
